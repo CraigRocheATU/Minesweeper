e085d0b1e49d75a8f1d61a26674d91ed70f9cca2
CraigRocheATU <g00422582@atu.ie>
2025-08-31 20:28:51 +0100

    Updated utils.c and utils.h for better functionality and error fixes

--- utils.h
+++ utils.h
@@ -6,9 +6,7 @@
 int read_int_in_range(const char* prompt, int min, int max);
 void read_string(const char* prompt, char* buf, int buflen);
 bool yes_no(const char* prompt);
-void press_continue(void);
 void clear_input_buffer(void);
-void trim_newline(char *s);
+void trim_newline(char* s);
 
 #endif // UTILS_H
-
